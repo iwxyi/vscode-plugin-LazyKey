a246fb8d482302a88eff96bf3a914405c4cd30d2
MRXY001 <wxy19980615@gmail.com>
2019-10-15 19:28:46 +0800

    支持多光标

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@ function activate(context) {
 		vscode.window.showInformationMessage('Lazy life starts!');
 	}));
 
-	require('./src/dotToPoint.js')(context);
+	require('./src/completions.js')(context);
 }
 exports.activate = activate;
 
@@ -22,63 +22,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-}
-
-function keyDot() {
-	// 读取设置是否进行更改
-	const conf = vscode.workspace.getConfiguration().get('LazyKey.dotToPoint');
-	// vscode.workspace.getConfiguration().update('LazyKey.dotToPoint', '懒键', true);
-	if (conf != true) return;
-
-	const editor = vscode.window.activeTextEditor;
-	if (editor.selection.text != undefined) return; // 有选中文本了
-
-	const document = editor.document;
-	const selection = editor.selection;
-	if (selection.start.line != selection.end.line || selection.start.character != selection.end.character)
-	{
-		// 插入 .
-		console.log('有选中');
-		return ;
-	}
-	var position = selection.active;
-
-	// 获取全文和单词
-	var full = document.getText();
-	var leftPosition = new vscode.Position(position.line, position.character);   // 左边单词右位置
-	var word = document.getText(document.getWordRangeAtPosition(leftPosition));  // 点号左边的单词
-
-	// 获取当前行内容
-	var line = document.lineAt(position).text;
-	var left = line.substring(0, leftPosition.character);
-	var right = line.substring(position.character);
-
-	// 两个点号变成指针
-	var doublePoint = false;
-	if (left.length >= 2 && left.slice(-1) == ".") {
-		if (left.slice(-2) == ".." || left.slice(-2) == "\t.") // 三个点或开头两点，不知道什么情况，退出
-			return;
-		if (left.slice(-2) == " .") // 针对可变参数数组的情况 ...
-			return;
-		leftPosition = new vscode.Position(leftPosition.line, leftPosition.character - 1);
-		word = document.getText(document.getWordRangeAtPosition(leftPosition));
-		left = line.substring(0, leftPosition.character - 1);
-		doublePoint = true; 0
-	}
-
-	// 判断上文是否有声明为 *var 或者 var-> 的字符
-	var re1 = new RegExp("\\*\\s*" + word + "\\b");
-	var re2 = new RegExp("\\b" + word + "\\s*->");
-	if (!doublePoint && !re1.test(full) && !re2.test(full))
-		return;
-
-	// 点号的位置替换为指针
-	var newEdit = vscode.TextEdit.replace(new vscode.Range(leftPosition, position), "->");
-
-	// 应该本次的修改
-	let textEdits = [];
-	textEdits.push(newEdit);
-	let wordspaceEdit = new vscode.WorkspaceEdit();
-	wordspaceEdit.set(document.uri, textEdits);
-	vscode.workspace.applyEdit(wordspaceEdit);
 }
--- src/keydot.js
+++ src/keydot.js
@@ -1,21 +1,26 @@
-/**
- * C++类 点号转指针
- * API：https://code.visualstudio.com/api/references/vscode-api
- */
 const vscode = require('vscode');
 
-function provideCompletionItems(document, position, token, context) {
+function keyDot() {
     // 读取设置是否进行更改
     const conf = vscode.workspace.getConfiguration().get('LazyKey.dotToPoint');
     // vscode.workspace.getConfiguration().update('LazyKey.dotToPoint', '懒键', true);
-    if (conf != true) return ;
+    if (conf != true) return;
 
     const editor = vscode.window.activeTextEditor;
     if (editor.selection.text != undefined) return; // 有选中文本了
 
+    const document = editor.document;
+    const selection = editor.selection;
+    if (selection.start.line != selection.end.line || selection.start.character != selection.end.character) {
+        // 插入 .
+        console.log('有选中');
+        return;
+    }
+    var position = selection.active;
+
     // 获取全文和单词
     var full = document.getText();
-    var leftPosition = new vscode.Position(position.line, position.character-1);   // 左边单词右位置
+    var leftPosition = new vscode.Position(position.line, position.character);   // 左边单词右位置
     var word = document.getText(document.getWordRangeAtPosition(leftPosition));  // 点号左边的单词
 
     // 获取当前行内容
@@ -23,29 +28,24 @@ function provideCompletionItems(document, position, token, context) {
     var left = line.substring(0, leftPosition.character);
     var right = line.substring(position.character);
 
-    // 判断左1是不是点号
-    if (line.slice(-1) != ".")
-        return ;
-
     // 两个点号变成指针
     var doublePoint = false;
-    if (left.length >= 2 && left.slice(-1) == ".")
-    {
+    if (left.length >= 2 && left.slice(-1) == ".") {
         if (left.slice(-2) == ".." || left.slice(-2) == "\t.") // 三个点或开头两点，不知道什么情况，退出
-            return ;
+            return;
         if (left.slice(-2) == " .") // 针对可变参数数组的情况 ...
-            return ;
+            return;
         leftPosition = new vscode.Position(leftPosition.line, leftPosition.character - 1);
         word = document.getText(document.getWordRangeAtPosition(leftPosition));
         left = line.substring(0, leftPosition.character - 1);
-        doublePoint = true;0
+        doublePoint = true; 0
     }
 
     // 判断上文是否有声明为 *var 或者 var-> 的字符
     var re1 = new RegExp("\\*\\s*" + word + "\\b");
-    var re2 = new RegExp("\\b"+word+"\\s*->");
+    var re2 = new RegExp("\\b" + word + "\\s*->");
     if (!doublePoint && !re1.test(full) && !re2.test(full))
-        return ;
+        return;
 
     // 点号的位置替换为指针
     var newEdit = vscode.TextEdit.replace(new vscode.Range(leftPosition, position), "->");
@@ -57,23 +57,3 @@ function provideCompletionItems(document, position, token, context) {
     wordspaceEdit.set(document.uri, textEdits);
     vscode.workspace.applyEdit(wordspaceEdit);
 }
-
-/**
- * 光标选中当前自动补全item时触发动作，一般情况下无需处理
- * @param {*} item
- * @param {*} token
- */
-function resolveCompletionItem(item, token) {
-    return null;
-}
-
-module.exports = function (context) {
-    // 注册代码建议提示，只有当按下“.”时才触发
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('cpp', {
-        provideCompletionItems,
-        resolveCompletionItem
-    }, '.'));
-    /*context.subscriptions.push(vscode.commands.registerCommand('extension.dotToPoint', function () {
-        console('dotdotdot');
-    }));*/
-};
