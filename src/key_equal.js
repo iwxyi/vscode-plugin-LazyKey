a72dd9098ffafed9ed00acfac2d4881f599bafd9
MRXY001 <wxy19980615@gmail.com>
2020-05-07 15:03:38 +0800

    修复等于时添加一个等号导致死循环出现提示框的问题

--- src/key_equal.js
+++ src/key_equal.js
@@ -11,8 +11,8 @@ const vscode = require('vscode');
 
 function provideCompletionItems(document, position, token, context) {
     // 读取设置是否进行开启
-    if (!(vscode.workspace.getConfiguration().get('LazyKey.AllEnabled'))
-        || !(vscode.workspace.getConfiguration().get('LazyKey.AutoOperators')))
+    if (!(vscode.workspace.getConfiguration().get('LazyKey.AllEnabled')) ||
+        !(vscode.workspace.getConfiguration().get('LazyKey.AutoOperators')))
         return;
     if (['c', 'cpp', 'java', 'js', 'javascript', 'jsp', 'php', 'cs'].indexOf(document.languageId) == -1)
         return;
@@ -25,12 +25,13 @@ function provideCompletionItems(document, position, token, context) {
     // 多个，有一个需要添加则进行添加
     var selections = editor.selections;
     let textEdits = [];
+    var showSuggestions = true;
     for (var i = selections.length - 1; i >= 0; --i) {
         // 获取全文和当前行内容
         position = selections[i].end;
         var full = document.getText();
-        var leftPosition = new vscode.Position(position.line, position.character - 1);   // 左边单词右位置
-        var word = document.getText(document.getWordRangeAtPosition(leftPosition));  // 点号左边的单词
+        var leftPosition = new vscode.Position(position.line, position.character - 1); // 左边单词右位置
+        var word = document.getText(document.getWordRangeAtPosition(leftPosition)); // 点号左边的单词
         var line = document.lineAt(position).text;
         var inpt = line.substring(position.character - 1, position.character);
         var left = line.substring(0, leftPosition.character);
@@ -67,7 +68,7 @@ function provideCompletionItems(document, position, token, context) {
         else if (/\S1$/.test(left)) {
             // 判断存不存在名字叫“var1”的变量
             var wordPos = new vscode.Position(position.line, position.character - 2);
-            var word = document.getText(document.getWordRangeAtPosition(wordPos));  // 左边的单词(包含末尾的1)
+            var word = document.getText(document.getWordRangeAtPosition(wordPos)); // 左边的单词(包含末尾的1)
             var match1 = full.match(new RegExp("\\b" + word, 'g'));
             if (match1 != null && match1.length > 1) { // 存在这样的变量
                 newText = " =";
@@ -82,7 +83,7 @@ function provideCompletionItems(document, position, token, context) {
         else if (/\S_$/.test(left)) {
             // 判断存不存在名字叫“var1”的变量
             var wordPos = new vscode.Position(position.line, position.character - 2);
-            var word = document.getText(document.getWordRangeAtPosition(wordPos));  // 左边的单词(包含末尾的1)
+            var word = document.getText(document.getWordRangeAtPosition(wordPos)); // 左边的单词(包含末尾的1)
             var match1 = full.match(new RegExp("\\b" + word, 'g'));
             if (match1 != null && match1.length > 1) { // 存在这样的变量
                 newText = " =";
@@ -113,6 +114,8 @@ function provideCompletionItems(document, position, token, context) {
             newText = '=';
             if (!right.startsWith(' '))
                 newText += ' ';
+            else
+                showSuggestions = false;
         }
         // var+=|    ==>    var += |
         else if (/(\b[\w_][\w\d_]*\b|\)|\])[\-\+\*\/%=<>]$/.test(left)) {
@@ -137,9 +140,8 @@ function provideCompletionItems(document, position, token, context) {
             newText = " ===";
             if (!right.startsWith(' '))
                 newText += ' ';
-        }
-        else { // 不知道什么情况
-            return ;
+        } else { // 不知道什么情况
+            return;
         }
 
         // 点号的位置替换为指针
@@ -153,9 +155,12 @@ function provideCompletionItems(document, position, token, context) {
     wordspaceEdit.set(document.uri, textEdits);
     vscode.workspace.applyEdit(wordspaceEdit);
 
+    // 是否需要提示判断
+    if (!showSuggestions)
+        return;
     // 延时出现提示（必须延时才会出现）
     if (vscode.workspace.getConfiguration().get('LazyKey.AutoSuggestion')) {
-        setTimeout(function () {
+        setTimeout(function() {
             vscode.commands.executeCommand('editor.action.triggerSuggest');
         }, 100);
     }
@@ -170,11 +175,10 @@ function resolveCompletionItem(item, token) {
     return null;
 }
 
-module.exports = function (context) {
+module.exports = function(context) {
     // 注册代码建议提示，只有当按下“.”时才触发
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
-        { scheme: 'file', languages: ['c', 'cpp', 'php', 'java', 'js', 'cs', 'python', 'jsp'] }, {
-            provideCompletionItems,
-            resolveCompletionItem
-        }, '='));
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ scheme: 'file', languages: ['c', 'cpp', 'php', 'java', 'js', 'cs', 'python', 'jsp'] }, {
+        provideCompletionItems,
+        resolveCompletionItem
+    }, '='));
 };
